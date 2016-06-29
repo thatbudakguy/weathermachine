89ea67ac116a2d6aabb67f42c16071e15e8119b2
Rishi Javia <rishi.javia.3895@gmail.com>
2016-06-29 11:15:58 -0700

    Compare existence of remote files to local files

--- weathermachine.py
+++ weathermachine.py
@@ -23,6 +23,18 @@ from oauth2client import tools
 FLAGS = argparse.ArgumentParser(
     parents=[tools.argparser],
     description='Upload files to Google Drive archive.')
+FLAGS.add_argument(
+    '-r',
+    '--root_dir',
+    type=str,
+    nargs=1,
+    help='Path to root directory containing all files to be uploaded.')
+FLAGS.add_argument(
+    '-v',
+    '--check_validity',
+    type=str,
+    nargs=1,
+    help='Google Drive folder ID to check against root directory for upload validity.')
 ARGS = FLAGS.parse_args()
 
 # Populate the CLIENT_SECRET_FILE using non-sensitive data from auth.json
@@ -70,6 +82,36 @@ def get_credentials():
         print('Storing credentials to ' + credential_path)
     return credentials
 
+def loop_local(root_dir_path, total=0):
+    """ Generate a map of the local directory structure """
+    result = []
+    for root, dirs, files in os.walk(root_dir_path):
+        for name in files:
+            if not name.startswith('.'):
+                result.append(name)
+                total += 1
+        for name in dirs:
+            if not name.startswith('.'):
+                result.append(name)
+                total += 1
+    return result, total
+
+def loop_drive(service, folder_id, result, total=0):
+    """ Generates a map of the google drive folder structure """
+    page_token = None
+    while True:
+        param = {'maxResults': 1000}
+        if page_token:
+            param['pageToken'] = page_token
+        children = service.files().list(q="'%s' in parents" % folder_id, **param).execute()
+        total += len(children.get('items', []))
+        for child in children.get('items', []):
+            result.append(child['title'])
+            result, total = loop_drive(service, child['id'], result, total)
+        page_token = children.get('nextPageToken')
+        if not page_token:
+            return result, total
+
 def main():
     """Main Function
 
@@ -85,6 +127,32 @@ def main():
     if len(sys.argv) == 1:
         FLAGS.print_help()
         sys.exit(1)
+
+    if ARGS.root_dir and ARGS.check_validity:
+        diff_items = []
+        root_dir_name = os.path.split(ARGS.root_dir[0])[1]
+        remote_dir = ARGS.check_validity[0]
+        remote_result, remote_total = loop_drive(service, remote_dir, [], 0)
+        local_result, local_total = loop_local(ARGS.root_dir[0], 0)
+        if remote_total == local_total:
+            print("Total of %d files match" % remote_total)
+        elif local_total > remote_total:
+            diff = local_total - remote_total
+            print("Missing files: %d files found locally missing from remote" % diff)
+            for item in local_result:
+                if item not in remote_result:
+                    diff_items.append(item)
+            for item in diff_items:
+                print(item)
+        elif remote_total > local_total:
+            diff = remote_total - local_total
+            print("Missing files: %d files found remotely missing from local machine" % diff)
+            for item in remote_result:
+                if item not in remote_result:
+                    diff_items.append(item)
+            for item in diff_items:
+                print(item)
+        sys.exit(0)
 
 if __name__ == '__main__':
     main()
